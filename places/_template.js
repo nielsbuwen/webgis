7681445056685c78558ca9907c632b33226ef6c2
Fabio Becker <Fabio@dhcp658-074.laptop-wlc.uni-heidelberg.de>
2018-10-25 14:37:05 +0200

    test commit

--- places/_template.js
+++ places/_template.js
@@ -2,7 +2,7 @@ add_polygon("HIER DEN NAMEN EINTRAGEN", 'HIER EINE FARBE EINTRAGEN', [
     // Hier die Koordinaten eintragen:
     // Das Polygon kann aus beliebig vielen Punkten zusammengesetzt werden.
     // Es sollte aber aus mindestens drei Punkten bestehen.
-    [LAT, LON],
-    [LAT, LON],
-    [LAT, LON]
+    [1, 2],
+    [3, 4],
+    [5, 6]
 ]);
