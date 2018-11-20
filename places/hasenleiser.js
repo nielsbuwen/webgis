a340b6bc3fa8e867ff1060586ee6c84a8e5652f2
Fabio Becker <fbo.becker@gmail.com>
2018-11-20 16:11:44 +0100

    add hasenleiser

---
+++ places/hasenleiser.js
@@ -0,0 +1,9 @@
+add_polygon("Hasenleiser", 'red', [
+    // Hier die Koordinaten eintragen:
+    // Das Polygon kann aus beliebig vielen Punkten zusammengesetzt werden.
+    // Es sollte aber aus mindestens drei Punkten bestehen.
+    [49.37688960487272, 8.679027557373049],
+    [49.37648445376879, 8.679059743881227],
+    [49.37644254139502, 8.678544759750368],
+	[49.37688261953716, 8.678523302078249]
+]);
