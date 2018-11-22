dc33e25d240e72bb20d1a6b482f5dc2fa056feb1
GeowazM <39764097+GeowazM@users.noreply.github.com>
2018-11-22 21:08:36 +0100

    add brücke ploygon via places/bruecke.js (#26)

---
+++ places/bruecke.js
@@ -0,0 +1,8 @@
+add_polygon("Bruecke", 'blue', [
+    // Hier die Koordinaten eintragen:
+    // Das Polygon kann aus beliebig vielen Punkten zusammengesetzt werden.
+    // Es sollte aber aus mindestens drei Punkten bestehen.
+    [49.41049852410589, 8.676774501800539],
+    [49.411252418487074, 8.677203655242922],
+    [49.40981442474044, 8.676860332489015]
+]);
